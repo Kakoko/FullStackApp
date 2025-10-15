0b534e4818b15f5797bdb66a1b993809df60d9e1
Simuzeche Kaluwa <simuzechek@gmail.com>
2025-10-15 18:48:41 +0200

    Feat : Added DbContext to the DI COntainer

--- FullStack.Api/Program.cs
+++ FullStack.Api/Program.cs
@@ -1,6 +1,14 @@
+using FullStack.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Configure EF Core - change connection string as needed
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
