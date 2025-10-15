ab7a2b23b99bd13e90d3f62c6cfe0552e46b4eb1
Simuzeche Kaluwa <simuzechek@gmail.com>
2025-10-15 18:45:24 +0200

    feat : Added Employee Model

---
+++ FullStack.Data/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStack.Data.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }// primary key
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Position { get; set; }
+        public DateTime DateHired { get; set; }
+    }
+}
